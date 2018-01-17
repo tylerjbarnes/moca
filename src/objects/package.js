dd8713f631c1eee00cfc446f2d862c8c2783efee
Tyler Barnes <tylerjbarnes@icloud.com>
2018-01-16 19:28:18 -0700

    revised package.js

--- src/objects/package.js
+++ src/objects/package.js
@@ -1,17 +1,24 @@
-import MocaObject from './mocaObject.js';
-import MocaMutationSet from '../objects/mocaMutationSet.js';
 import MocaFactory from '../objects/mocaFactory.js';
+import MocaMutationSet from '../objects/mocaMutationSet.js';
+import MocaObject from './mocaObject.js';
 
 class Package extends MocaObject {
 
-    get time () {
-        return store.getters.creditForPackage(this.id);
-    }
+    // related objects
 
     get client () {
         return store.getters.person(this.client_id);
     }
 
+    get time () {
+        return store.getters.creditByPackage(this.id);
+    }
+
+    // actions
+
+    /**
+     * expire package
+     */
     expire () {
         let primitive = MocaFactory.constructPrimitive('time', {
             type: 'expiration',
@@ -25,6 +32,10 @@ class Package extends MocaObject {
         ).commit();
     }
 
+    /**
+     * extend package
+     * @param  {string} dateString
+     */
     extend (dateString) {
         new MocaMutationSet(
             'update', 'package',
