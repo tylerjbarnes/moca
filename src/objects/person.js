2911f9dc2d8e1f0efab94d1145a59c4f3c3f5559
Tyler Barnes <tylerjbarnes@icloud.com>
2018-01-16 19:33:58 -0700

    revised person.js

--- src/objects/person.js
+++ src/objects/person.js
@@ -1,17 +1,23 @@
-import MocaObject from './mocaObject.js';
 import MocaMutationSet from '../objects/mocaMutationSet.js';
+import MocaObject from './mocaObject.js';
 
 class Person extends MocaObject {
 
-    constructor (personPrimitive) {
-        super(personPrimitive);
-        this.online = false;
-    }
+    // computed properties
 
     get firstName () {
         return this.name.split(' ')[0];
     }
 
+    get online () {
+        return store.getters.online(this.id);
+    }
+
+    // actions
+
+    /**
+     * archive person
+     */
     archive () {
         new MocaMutationSet(
             'update', 'person',
@@ -21,6 +27,9 @@ class Person extends MocaObject {
         ).commit();
     }
 
+    /**
+     * unarchive person
+     */
     unarchive () {
         new MocaMutationSet(
             'update', 'person',
