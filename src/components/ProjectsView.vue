d7c83fdd0e28ba8d4328e0e9407440c9577b3d7b
Tyler Barnes <tylerjbarnes@icloud.com>
2018-02-19 00:13:18 -0700

    made projects tab fancy

--- src/components/ProjectsView.vue
+++ src/components/ProjectsView.vue
@@ -2,14 +2,44 @@
 
     <div class="projects-view">
         <template v-if="$store.getters.user.canManage">
-            <h1 class="title">Undelegated</h1>
-            <project-collection :fluid="true" :projects="undelegatedProjects"></project-collection>
+            <div class="section-header" :class="{open: delegateOpen}" @click="delegateOpen = !delegateOpen">
+                <h1 class="title">Delegate</h1>
+                <span class="count">{{ undelegatedProjects.length }}</span>
+                <icon scale="1.2" name="chevron-right"></icon>
+            </div>
+            <project-collection v-show="delegateOpen && undelegatedProjects.length" :fluid="true" :projects="undelegatedProjects"></project-collection>
+            <div class="section-header" :class="{open: doOpen}" @click="doOpen = !doOpen">
+                <h1 class="title">Do</h1>
+                <span class="count">{{ activeProjects.length }}</span>
+                <icon scale="1.2" name="chevron-right"></icon>
+            </div>
+            <project-collection v-show="doOpen && activeProjects.length" :fluid="true" :projects="activeProjects"></project-collection>
+            <div class="section-header" :class="{open: approveOpen}" @click="approveOpen = !approveOpen">
+                <h1 class="title">Approve</h1>
+                <span class="count">{{ pendingProjects.length }}</span>
+                <icon scale="1.2" name="chevron-right"></icon>
+            </div>
+            <project-collection v-show="approveOpen && pendingProjects.length" :fluid="true" :projects="pendingProjects"></project-collection>
+            <div class="section-header" :class="{open: sendOpen}" @click="sendOpen = !sendOpen">
+                <h1 class="title">Send</h1>
+                <span class="count">{{ approvedProjects.length }}</span>
+                <icon scale="1.2" name="chevron-right"></icon>
+            </div>
+            <project-collection v-show="sendOpen && approvedProjects.length" :fluid="true" :projects="approvedProjects"></project-collection>
         </template>
         <template v-else>
-            <h1 class="title" v-if="activeProjects.length">Active</h1>
-            <project-collection :fluid="true" :projects="activeProjects"></project-collection>
-            <h1 class="title" v-if="pendingProjects.length">Pending Approval</h1>
-            <project-collection :fluid="true" :projects="pendingProjects"></project-collection>
+            <div class="section-header" :class="{open: doOpen}" @click="doOpen = !doOpen">
+                <h1 class="title">Do</h1>
+                <span class="count">{{ activeProjects.length }}</span>
+                <icon scale="1.2" name="chevron-right"></icon>
+            </div>
+            <project-collection v-show="doOpen && activeProjects.length" :fluid="true" :projects="activeProjects"></project-collection>
+            <div class="section-header" :class="{open: approveOpen}" @click="approveOpen = !approveOpen">
+                <h1 class="title">Pending Approval</h1>
+                <span class="count">{{ pendingProjects.length }}</span>
+                <icon scale="1.2" name="chevron-right"></icon>
+            </div>
+            <project-collection v-show="approveOpen && pendingProjects.length" :fluid="true" :projects="pendingProjects"></project-collection>
         </template>
     </div>
 
@@ -25,7 +55,11 @@
         name: 'projects-view',
         components: {ProjectCollection},
         data () {return {
-            statuses: ['delegate','do','approve','send']
+            statuses: ['delegate','do','approve','send'],
+            delegateOpen: true,
+            doOpen: false,
+            approveOpen: false,
+            sendOpen: false
         }},
         computed: {
             allProjects () {
@@ -41,9 +75,28 @@
             },
             undelegatedProjects () {
                 return this.projects.filter(x => x.status == 'delegate');
+            },
+            approvedProjects () {
+                return this.projects.filter(x => x.status == 'send');
             }
         },
-        mixins: [CanSearchProjects,DragDropController]
+        mixins: [CanSearchProjects,DragDropController],
+        mounted () {
+            let savedScroll = localStorage['projectsScroll'];
+            document.body.scrollTop = savedScroll;
+        },
+        created () {
+            if (localStorage.getItem('delegateOpen') == 1) this.delegateOpen = true;
+            if (localStorage.getItem('doOpen') == 1) this.doOpen = true;
+            if (localStorage.getItem('approveOpen') == 1) this.approveOpen = true;
+            if (localStorage.getItem('sendOpen') == 1) this.sendOpen = true;
+        },
+        watch: {
+            delegateOpen: function(val) { localStorage.setItem('delegateOpen', val ? 1 : 0); },
+            doOpen: function(val) { localStorage.setItem('doOpen', val ? 1 : 0); },
+            approveOpen: function(val) { localStorage.setItem('approveOpen', val ? 1 : 0); },
+            sendOpen: function(val) { localStorage.setItem('sendOpen', val ? 1 : 0); }
+        }
     }
 
 </script>
@@ -56,12 +109,35 @@
 
         h1 {
             font-weight: 700;
-            padding: 40px 40px 0 40px;
+            padding: 40px 10px 0 40px;
 
             &.missing {
                 font-size: 2em;
                 text-align: center;
                 opacity: 0.5;
+            }
+
+        }
+
+        span.count {
+            font-size: 1.6em;
+            font-weight: 300 !important;
+            opacity: 0.5;
+            padding-right: 10px;
+        }
+
+        .section-header {
+            align-items: baseline;
+            cursor: default;
+            display: flex;
+            &.open .fa-icon {
+                transform: rotateZ(90deg);
+            }
+
+            .fa-icon {
+                opacity: 0.75;
+                transition: 0.15s ease-out;
+
             }
 
         }
